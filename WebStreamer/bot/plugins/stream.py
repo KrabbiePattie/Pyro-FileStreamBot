a249589bcd49bf1088d98b4590f3ff3fea0c7d21
KrabbiePattie <70745660+KrabbiePattie@users.noreply.github.com>
2022-09-01 15:35:09 +0100

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -81,7 +81,7 @@ async def private_receive_handler(c: Client, m: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("📩 DIRECT DOWNLOAD 📩", url=stream_link)],
-                    [InlineKeyboardButton("FREE NETFLIX ACCOUNTS 💯", url="https://t.me/+xPvyu36YNV83YWVk")]),
+                    [InlineKeyboardButton("FREE NETFLIX ACCOUNTS 💯", url="https://t.me/+xPvyu36YNV83YWVk")],
                 ]
             quote=True
         )
